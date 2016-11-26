edd81f115ca6bbbca772b8dee58933d135c9020e
guillaug-osu <guillaug@onid.oregonstate.edu>
2016-11-26 23:25:03 +0000

    Development - install files necessary for express

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,6 @@
+var express = require('express');
+var router = express.Router();
+
 var forms = require('forms');
 var fields = forms.fields;
 var validators = forms.validators;
@@ -12,23 +15,11 @@ var reg_form = forms.create({
     email: fields.email()
 });
 
-console.log(reg_form.toHTML())
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
- 
-db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
- 
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
- 
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+    console.log("WE GOING!");
+    res.sendfile('./views/login.html');
 });
- 
-db.close();
+
+module.exports = router;
