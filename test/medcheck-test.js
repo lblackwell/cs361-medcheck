f14f890705774d2a2bc69010353ae0471ef4ffe7
Lucia Blackwell <lblackwe@udel.edu>
2016-12-03 19:24:39 -0800

    Added additional login tests

--- test/medcheck-test.js
+++ test/medcheck-test.js
@@ -6,12 +6,25 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
-// Check that login posts
 
-// Check that a correct username/password grants access
-
-// Check that an incorrect password blocks access
+// Login //////////////////////////////////////////////
 describe('/POST login', () => {
+	// Correct username/password grants access
+	it('it should POST a login with correct username and password', (done) => {
+		let login = {
+			username: "testuser",
+			password: "password"
+		}
+	chai.request(server).post('/user').send(login).end((err, res) => {
+		res.should.have.status(200);
+		res.body.should.be.a('object');
+		res.body.should.have.property('message').eql('Logged in successfully!');
+		res.body.login.should.have.property('firstname');
+		done();
+		});
+	});
+
+	// Incorrect password blocks access
 	it('it should not allow a login with an incorrect password', (done) => {
 		let login = {
 			username: "testuser",
@@ -27,7 +40,7 @@ describe('/POST login', () => {
 		});
 	});
 
-// Check that a blank password blocks access
+	// Blank password blocks access
 	it('it should not POST a login without password entered', (done) => {
 		let login = {
 			username: "testuser"
@@ -41,6 +54,30 @@ describe('/POST login', () => {
 		done();
 		});
 	});
+
+	// Non-registered username blocks access
+	it('it should not allow a login with a non-registered username', (done) => {
+		let login = {
+			username: "incorrect",
+			password: "password"
+		}
+	chai.request(server).post('/user').send(login).end((err, res) => {
+		res.should.have.status(200);
+		res.body.should.be.a('object');
+		res.body.should.have.property('errors');
+		res.body.errors.should.have.property('username');
+		res.body.errors.username.should.have.property('kind').eql('unregistered');
+		done();
+		});
+	})
+
 });
 
-// Check that a non-registered username blocks access
+
+// Search /////////////////////////////////////////////
+
+	// Search does not execute without a term entered
+
+	// Search with matching term returns appropriate match
+
+	// Search without matching term returns nothing
